0040c207b45e45598ba7235f6e428043f54162ad
Joshua Parker <10247779@uvu.edu>
2018-11-14 21:39:48 -0700

    tweak the answer format a bit

--- scrape-wiki-cats.js
+++ scrape-wiki-cats.js
@@ -31,13 +31,25 @@ const getCategory = term => {
             })
         )
     })
-    .then(e => {
-        console.log(e);
+    .then(res => {
+        let output = res.map((e,i) => {
+            let a = list[i].attribs.href.slice(6).replace(/_/g,' ');
+            let are = new RegExp(a.replace(/ /g,'.'),'gi');
+            console.log(are);
+            return {
+                text: e.replace(are,'********'),
+                answer: a
+            }
+        });
+        console.log(output);
+        fs.writeFile(
+            `./${keyword}scrape-wiki-cats.json`,
+            JSON.stringify(output),err => {
+                console.error(err)});
     })
     .catch(err => {
         console.error(err);
     })
-
 }
 
 getCategory(keyword);
